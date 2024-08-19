f27c6dc0a8f173e4ec8b99be12970c9aa9621441
Shawn Song <sonykey2003@gmail.com>
2024-08-19 11:55:30 +0800

    added new feature and more formatting

--- myCorpInsightTool.py
+++ myCorpInsightTool.py
@@ -19,18 +19,51 @@ def main():
     parser.add_argument('domain', type=str, help='Domain to discover')
     parser.add_argument('--all', action='store_true', help='Display full dataset')
     parser.add_argument('--depthc', action='store_true', help='Display departmental_head_count dataset')
+    parser.add_argument('--raw', action='store_true', help='Display the raw JSON dataset')
 
     args = parser.parse_args()
 
     apikey = get1Pkey("op://Personal/Apollo.io/credential")
     services = corpDiscovery(args.domain, apikey)
+    # Formatting
+    bold_start = '\033[1m'
+    bold_end = '\033[0m'
+    color_start = '\033[92m'  # Green
+    color_end = '\033[0m'
+
+    # Group technologies by category
+    categories = {}
+
+    # For a streamlined view
+    desired_categories = [
+        'Hosting', 
+        'Email Delivery', 
+        'Cloud Services', 
+        'CMS', 
+        'Enterprise Mobility Management'
+    ]
+
+    # Building SaaS category
+    technologies = services['organization']['current_technologies']
+    for tech in technologies:
+        category = tech['category']
+        service_name = tech['name']
+        if category not in categories:
+            categories[category] = []
+        categories[category].append(service_name)
+    
+    # Printing out info
+    email_providers = [tech['name'] for tech in services['organization']['current_technologies'] if tech['category'] == 'Email Providers']
+    print(f"\n\nEmail service is:  {email_providers}\n")
+    print(f"Total Estimated Employees:  {services['organization']['estimated_num_employees']}\n")
     
     if args.all:
-        print(json.dumps(services, indent=2))
+        # Print out the categories with services
+        print(f"Here is a list of SaaS {args.domain} is using:\n")
+        for category, services in categories.items():
+            print(f"{bold_start}{color_start}{category}:{bold_end}{color_end} {', '.join(services)}")
+
     elif args.depthc:
-        email_providers = [tech['name'] for tech in services['organization']['current_technologies'] if tech['category'] == 'Email Providers']
-        print(f"\n\nEmail service is:  {email_providers}\n")
-        print(f"Total Estimated Employees:  {services['organization']['estimated_num_employees']}\n")
         print(f"Here is a list of SaaS {args.domain} is using:\n")
         for svc in services['organization']['technology_names']:
             print(svc)
@@ -38,14 +71,16 @@ def main():
         print(json.dumps(services['organization']['departmental_head_count'], indent=2))
         print("\n\n\n")
 
+    elif args.raw:
+        print(json.dumps(services, indent=2))
+        
     else:
-        email_providers = [tech['name'] for tech in services['organization']['current_technologies'] if tech['category'] == 'Email Providers']
-        print(f"\n\nEmail service is:  {email_providers}\n")
-        print(f"Total Estimated Employees:  {services['organization']['estimated_num_employees']}\n")
-        print(f"Here is a list of SaaS {args.domain} is using:\n")
-        for svc in services['organization']['technology_names']:
-            print(svc)
-        print("\n\n\n")
-
+        print(f"Here is a top list of SaaS {args.domain} is using:\n")
+        
+       
+        for category, services in categories.items():
+            if category in desired_categories:
+               print(f"{bold_start}{color_start}{category}:{bold_end}{color_end} {', '.join(services)}")
+        
 if __name__ == "__main__":
     main()
